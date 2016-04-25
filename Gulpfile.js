6d5bd7f98fb38493a8e129dc5a5e37865f33414e
Pete Lambert <pete@petelambert.com>
2016-04-25 13:29:50 +0100

    Changed Gulp to watch SASS input rather than output

--- Gulpfile.js
+++ Gulpfile.js
@@ -36,7 +36,7 @@ gulp.task('watch', function() {
   return gulp
     // Watch the input folder for change,
     // and run `sass` task when something happens
-    .watch(output, ['sass'])
+    .watch(input, ['sass'])
     // When there is a change,
     // log a message in the console
     .on('change', function(event) {

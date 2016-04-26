f28338935fa16c8e6c75fee046af6a30151fc7c9
Pete Lambert <pete@petelambert.com>
2016-04-26 12:26:38 +0100

    Ugh. Refactored how times are done and I think I've got it correct now.

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from __future__ import print_function # In python 2.7
+import sys
 from flask import Flask, render_template
 import forecastio
 from datetime import datetime, timedelta
@@ -10,55 +12,71 @@ import random
 
 app = Flask(__name__)
 
+# Returns the UTC date
+def utcnow():
+    return datetime.datetime.now()
+
+def whatdayisit():
+    now = utcnow()
+    return now.strftime("%A")
+
+fmt = '%-I%p'
+local_timezone = timezone('GMT')
+local_datetime = local_timezone.localize(utcnow())
+local_datetime_readable = local_timezone.localize(utcnow()).strftime(fmt)
+
+def rideTime(club): 
+    day = whatdayisit()
+    current_time = local_datetime
+    if club == "vcyork":
+        if day == 'Tuesday' or day == 'Thursday':
+            ride_time = local_datetime.replace(hour=18, minute=00, second=00)
+        elif day == 'Saturday' or day == 'Sunday':
+            ride_time = local_datetime.replace(hour=9, minute=00, second=00)
+    else:
+        ride_time = current_time
+    
+    ride_time_seconds = ride_time.astimezone(local_timezone).replace(tzinfo=None)
+    current_time_seconds = current_time.astimezone(local_timezone).replace(tzinfo=None)
+    ride_time_diff = current_time_seconds - ride_time_seconds
+    ride_time_diff = ride_time_diff.total_seconds()
+    
+    print(ride_time, file=sys.stderr)
+    
+    if ride_time_diff > 60: 
+        ride_time = current_time
+    return ride_time
+
+def getRandomLocation():
+    magic_locations = ['Tourmalet, France', 'Gavia, Italy', 'Roubaix Velodrome, Belgium', 
+                       "L'Alpe d'Huez, France", 'Mont Ventoux, France', 
+                       'Col du Galibier, France', 'Tan Hill, UK', 'Buttertubs, UK', 
+                       'Holme Moss, UK', 'Rosedale Chimney, UK', 'Liege, Belgium']
+    return random.choice(magic_locations)
+
 optLat = 53.9591
 optLng = -1.0815
-optLocation = "York, UK"
+optLocation = getRandomLocation()
 optUnits = 'uk'
 
-def getTheWeather(optLat, optLng, optUnits, club=''):
-    # Returns the UTC date
-    def utcnow():
-        return datetime.datetime.now()
 
-    def whatdayisit():
-        now = utcnow()
-        return now.strftime("%A")
-         
-    fmt = '%-I%p'
-    local_timezone = timezone('GMT')
-    local_datetime = local_timezone.localize(utcnow())
-    local_datetime_readable = local_timezone.localize(utcnow()).strftime(fmt)
-    # ride_time_diff = 123
 
-    def rideTime(): 
-        day = whatdayisit()
-        current_time = local_datetime
-        if club == "vcyork":
-            if day == 'Tuesday' or day == 'Thursday':
-                ride_time = local_datetime.replace(hour=18, minute=00, second=00)
-            elif day == 'Saturday' or day == 'Sunday':
-                ride_time = local_datetime.replace(hour=9, minute=00, second=00)
-        else:
-            ride_time = current_time
-        
-        ride_time_seconds = ride_time.astimezone(local_timezone).replace(tzinfo=None)
-        current_time_seconds = current_time.astimezone(local_timezone).replace(tzinfo=None)
-        ride_time_diff = current_time_seconds - ride_time_seconds
-        ride_time_diff = ride_time_diff.total_seconds()
-        
-        if ride_time_diff > 60: 
-            ride_time = current_time
-        return ride_time
+def getTheWeather(optLat=optLat, optLng=optLng, optUnits=optUnits, optStartTime=None, optStartLocationName='', club=''):
+    
 
     def rideStart(): 
-        day = whatdayisit()
-        ride_time = rideTime()     
-        current_time = local_datetime
-        if ride_time.strftime('%b %d %Y %H:%M:%S') != current_time.strftime('%b %d %Y %H:%M:%S'):   
-            if day == 'Tuesday' or day == 'Thursday':
-                ride_start = "Today's ride leaves %s from B&Q. " % ride_time.strftime(fmt)
-            elif day == 'Saturday' or day == 'Sunday':
-                ride_start = "Today's ride leaves %s from the shelter. " % ride_time.strftime(fmt)
+        ride_start = ''
+        if optStartTime and club:
+            day = whatdayisit()
+            ride_time = optStartTime
+            rideStartLocationName = optStartLocationName 
+            current_time = local_datetime 
+            
+            if ride_time.strftime('%b %d %Y %H:%M:%S') != current_time.strftime('%b %d %Y %H:%M:%S'):   
+                if day == 'Tuesday' or day == 'Thursday':
+                    ride_start = "Today's ride leaves %s from B&Q. " % ride_time.strftime(fmt)
+                elif day == 'Saturday' or day == 'Sunday':
+                    ride_start = "Today's ride leaves %s from the shelter. " % ride_time.strftime(fmt)
         else:
             ride_start = 'Riding now? '
         return ride_start
@@ -73,7 +91,7 @@ def getTheWeather(optLat, optLng, optUnits, club=''):
     directions = ["N", "NNE", "ENE", "E", "ESE", "SSE", "S", "SSW", "WSW", "W", "WNW", "NNW"]
     def bearing_to_direction(bearing):
         d = 360. / 12.
-        return directions[int((bearing+d/2)/d)] 
+        return directions[int(((bearing+d/2)/d)-1)] 
     
     def winddir_text(pts):
         "Convert wind direction from 0..15 to compass point text"        
@@ -88,12 +106,12 @@ def getTheWeather(optLat, optLng, optUnits, club=''):
         thursdayTailwinds = ['East', 'ENE', 'ESE', 'SE']
         if whatdayisit() == 'Tuesday' and speed > 7:
             if windBearing in tuesdayTailwinds:
-                return 'Tailwind home!'
+                return ' Tailwind home!'
             else:
                 return
         elif whatdayisit() == 'Thursday' and speed > 7:
             if windBearing in thursdayTailwinds:
-                return 'Tailwind home!'
+                return ' Tailwind home!'
             else:
                 return
         else:
@@ -146,8 +164,7 @@ def getTheWeather(optLat, optLng, optUnits, club=''):
         return weatherIcon
         
         
-    def showTheWeather():
-
+    def showTheWeather(tailWind=None):
         theWeather = rideStart()
         theWeather = theWeather + "The forecast is %s and %s" % (weatherSummary, temperature)
         theWeather = theWeather + "The wind will blow from the %s at %s." % (windBearing, strWindSpeed)
@@ -160,8 +177,11 @@ def getTheWeather(optLat, optLng, optUnits, club=''):
     lat = optLat
     lng = optLng
     units = optUnits
-    current_time = local_datetime.isoformat()
-    ride_time = rideTime()
+    current_time = local_datetime
+    if optStartTime:
+        ride_time = optStartTime
+    else:
+        rideTime = current_time
     forecast = forecastio.load_forecast(api_key, lat, lng, time=ride_time, units=units)
 
     #Get the weather
@@ -172,16 +192,12 @@ def getTheWeather(optLat, optLng, optUnits, club=''):
     windSpeed = byCurrently.windSpeed
     strWindSpeed = str(int(round(byCurrently.windSpeed))) +  'mph'
     weatherIcon = weather_emoji(byCurrently.icon)
-    tailWind = tail_wind(windSpeed, windBearing)
+    tailWind = None
+    if club == 'vcyork':
+        tailWind = tail_wind(windSpeed, windBearing)
         
-    return showTheWeather()
+    return showTheWeather(tailWind=tailWind)
     
-def getRandomLocation():
-    magic_locations = ['Tourmalet, France', 'Gavia, Italy', 'Roubaix Velodrome, Belgium', 
-                       "L'Alpe d'Huez, France", 'Mont Ventoux, France', 
-                       'Col du Galibier, France', 'Tan Hill, UK', 'Buttertubs, UK', 
-                       'Fleet Moss, UK', 'Holme Moss, UK', 'Rosedale Chimney, UK', 'Liege, Belgium']
-    return random.choice(magic_locations)
         
 def getLatLng(location):
     location = geocoder.google(location)
@@ -210,8 +226,8 @@ def getLatLng(location):
 
 @app.route("/")
 def main():
-    location = getLatLng(optLocation)
-    weather = getTheWeather(location[1], location[2], optUnits)
+    location = getLatLng('York, UK')
+    weather = getTheWeather(location[1], location[2], optUnits, local_datetime)
     charcount = len(weather)
     return render_template('index.html', weather=weather, charcount=charcount, locationName=location[0], locationLat=location[1], locationLng=location[2])
     
@@ -219,7 +235,7 @@ def main():
 @app.route("/location/<location>")
 def local(location):
     location = getLatLng(location)
-    weather = getTheWeather(location[1], location[2], optUnits)
+    weather = getTheWeather(location[1], location[2], optUnits, local_datetime)
     charcount = len(weather)
     return render_template('index.html', weather=weather, charcount=charcount, locationName=location[0], locationLat=location[1], locationLng=location[2])
     
@@ -228,10 +244,13 @@ def local(location):
 def club(clubname):
     if clubname == "vcyork":
         location = getLatLng('York, UK')
+        startLocation = "B&Q"
+        startTime = rideTime(clubname)
+        weather = getTheWeather(location[1], location[2], optUnits, startTime, startLocation, clubname)  
     else:
-        location = getLatLng('Paris, France')
+        location = getLatLng(getRandomLocation())
+        weather = getTheWeather(location[1], location[2], optUnits, local_datetime)
     
-    weather = getTheWeather(location[1], location[2], optUnits)
     charcount = len(weather)
     return render_template('index.html', weather=weather, charcount=charcount, locationName=location[0], locationLat=location[1], locationLng=location[2])
     
@@ -239,14 +258,14 @@ def club(clubname):
 @app.errorhandler(404)
 def page_not_found(e):
     location = getLatLng(getRandomLocation())
-    weather = getTheWeather(location[1], location[2], optUnits)
+    weather = getTheWeather(location[1], location[2], optUnits, local_datetime)
     charcount = len(weather)
     return render_template('error.html', weather=weather, charcount=charcount, locationName=location[0], locationLat=location[1], locationLng=location[2]), 404
     
 @app.errorhandler(500)
 def server_error(e):
     location = getLatLng(getRandomLocation())
-    weather = getTheWeather(location[1], location[2], optUnits)
+    weather = getTheWeather(location[1], location[2], optUnits, local_datetime)
     charcount = len(weather)
     return render_template('error.html', weather=weather, charcount=charcount, locationName=location[0], locationLat=location[1], locationLng=location[2]), 500
     
